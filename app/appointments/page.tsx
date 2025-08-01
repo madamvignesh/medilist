ab00155bd25a6fcf9462c173c7643dde65dff587
Madam Vignesh <madamvignesh@gmail.com>
2025-08-01 15:15:28 +0530

    Updating management page v4.4

--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -47,31 +47,33 @@ const AppointmentsPage = () => {
         fetchAppointments();
     }, []);
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
 
     return (
         <div className="flex flex-col p-5 pt-25 bg-ebony-950 text-white min-h-screen">
             <h1 className="text-2xl font-bold mb-4">Appointments</h1>
             <ul className="gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-                {appointments && appointments.map((appointment) => (
-                    <li key={appointment.id} className="bg-ebony-900/40 p-4 rounded-lg shadow-md flex flex-row justify-between items-center hover:bg-ebony-400/30 transition-colors">
-                        <div>
-                            <span className="text-ebony-50 font-bold">Doctor: {appointment.doctor_name}</span> <br />
-                            <p className="text-md text-ebony-100">{appointment.name} - {appointment.datetime} </p>
-                            <span className="text-blue-102">({appointment.email})</span>
-                        </div>
-                        <div>
-                            <button
-                                onClick={() => deleteAppointment(appointment.id)}
-                                className="mt-2 bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600/40 transition-colors"
-                            >
-                                <CopyX />
-                            </button>
-                        </div>
-                    </li>
-                ))}
+                {loading && <li>Loading...</li>}
+                {!loading && appointments.length > 0 ? (
+                    appointments.map((appointment) => (
+                        <li key={appointment.id} className="bg-ebony-900/40 p-4 rounded-lg shadow-md flex flex-row justify-between items-center hover:bg-ebony-400/30 transition-colors">
+                            <div>
+                                <span className="text-ebony-50 font-bold">Doctor: {appointment.doctor_name}</span> <br />
+                                <p className="text-md text-ebony-100">{appointment.name} - {appointment.datetime} </p>
+                                <span className="text-blue-102">({appointment.email})</span>
+                            </div>
+                            <div>
+                                <button
+                                    onClick={() => deleteAppointment(appointment.id)}
+                                    className="mt-2 bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600/40 transition-colors"
+                                >
+                                    <CopyX />
+                                </button>
+                            </div>
+                        </li>
+                    ))
+                ) : (
+                    <div className="text-center text-gray-500">No appointments found</div>
+                )}
             </ul>
         </div>
     );
