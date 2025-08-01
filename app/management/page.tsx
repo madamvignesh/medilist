7fc9c5fb6d9fd10e138ad9299f29cde9aad2ad50
Madam Vignesh <madamvignesh@gmail.com>
2025-08-01 14:18:26 +0530

    Updating v6

--- app/management/page.tsx
+++ app/management/page.tsx
@@ -4,7 +4,14 @@ import { useState, useEffect, useMemo } from "react";
 import { Search } from "lucide-react";
 
 const ManagementPage = () => {
-    const [doctors, setDoctors] = useState([]);
+    interface Doctor{
+        id: string;
+        name: string;
+        email: string;
+        datetime: string;
+        doctor_name: string;
+    }
+    const [doctors, setDoctors] = useState<Doctor[]>([]);
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState("");
 
