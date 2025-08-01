24fac4c6cc03d0e11ecf04a72faa340def8fc687
Madam Vignesh <madamvignesh@gmail.com>
2025-08-01 14:14:35 +0530

    Updating v5

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useState, useEffect, useMemo } from "react";
 import { Search, X } from "lucide-react";
-import Stepper, { Step } from "../components/stepper/stepper.tsx";
+import Stepper, { Step } from "../components/stepper/stepper";
 
 const DoctorsPage = () => {
     const [doctors, setDoctors] = useState([]);
