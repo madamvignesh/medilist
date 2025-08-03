430f870b27988b8db150c2930fa30efb53fa33d6
Madam Vignesh <madamvignesh@gmail.com>
2025-08-03 13:45:06 +0530

    Updating Footer content

--- app/components/Footer/page.tsx
+++ app/components/Footer/page.tsx
@@ -16,8 +16,8 @@ const Footer: React.FC = () => (
         <ul>
           <li><Link href="/" className="hover:underline">Home</Link></li>
           <li><Link href="/doctors" className="hover:underline">Doctors</Link></li>
-          <li><Link href="/about" className="hover:underline">About</Link></li>
-          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
+          <li><Link href="/appointments" className="hover:underline">Appointments</Link></li>
+          <li><Link href="/management" className="hover:underline">Management</Link></li>
         </ul>
       </div>
       {/* Legal & Social */}
